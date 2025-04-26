620ca12920c6779c54708cb70d3147446ef1b709
Work <work@QBook-Pro.local>
2025-04-26 16:54:53 -0300

    fixing

---
+++ BTHOWeN/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BTHOWeN",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # List dependencies here
+        "numpy",
+        "matplotlib",
+        # Add other dependencies as needed
+    ],
+)
